bced34ba946e121efc3a918c732c69954cb9cdb0
Matt <matthew.morley.ca@gmail.com>
2024-01-06 21:59:35 -0700

    Yeet skeet

--- src/test/java/org/photonvision/rknn/RknnTest.java
+++ src/test/java/org/photonvision/rknn/RknnTest.java
@@ -18,15 +18,21 @@
 package org.photonvision.rknn;
 
 import java.io.IOException;
+import java.util.Arrays;
 
+import org.junit.jupiter.api.Test;
 import org.opencv.core.Mat;
 import org.opencv.core.Size;
 import org.opencv.dnn.Dnn;
 import org.opencv.imgcodecs.Imgcodecs;
 
 public class RknnTest {
-    public static void main(String[] args) throws IOException {
+    @Test
+    public void testBasicBlobs() throws IOException {
+        // I really really hate this. Someone should fix this for me
         System.load("/home/matt/Documents/GitHub/photonvision/photon-targeting/build/NativeMain/RawRuntimeLibs/linux/x86-64/shared/libopencv_java480.so");
+
+        // This also needs to get fixed
         System.load("/home/matt/Documents/rknn/rknn_java/cmake_build/librknn_java.so");
 
         Mat img = Imgcodecs.imread("src/test/resources/bus.jpg");
@@ -34,10 +40,13 @@ public class RknnTest {
 
         var blob = Dnn.blobFromImage(img, 1.0 / 255.0, new Size(640, 640));
 
+        // This also needs to get changed
         long ptr = RknnJNI.create("/home/matt/Documents/rknn/rknn_java/src/test/resources/yolov7-tiny.onnx");
         System.out.println(ptr);
+
         var ret = RknnJNI.detect(ptr, blob.getNativeObjAddr(), 0, 0, 1);
-        System.out.println(ret);
+        System.out.println(Arrays.toString(ret));
+
         RknnJNI.destroy(ptr);
 
         System.out.println(blob);
